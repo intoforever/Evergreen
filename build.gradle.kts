e000c1aae72020c5dc223ff7712fdb505f448aea
intoforever <startingfrom33@gmail.com>
2025-06-01 21:12:25 +0900

    #Evergreen
    - [cfg] 프로젝트 root의 Gradle 파일 정리

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,36 +1,50 @@
 plugins {
-    kotlin("jvm") version libs.versions.kotlin.jvm.get() apply false
-    id("org.springframework.boot") version libs.versions.spring.boot.get() apply false
-    id("io.spring.dependency-management") version libs.versions.spring.dependency.management.get() apply false
+    kotlin("jvm") version "1.9.0" apply false
+    id("java")
+    id("org.springframework.boot") version "3.2.5" apply false
+    id("io.spring.dependency-management") version "1.1.4" apply false
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+allprojects {
+    group = "com.evergreen"
+    version = "0.0.1-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
 
 subprojects {
+    // ────────────────────────────────────────────────────────────────────────
+    // 1) 공통 플러그인
+    // ────────────────────────────────────────────────────────────────────────
     apply(plugin = "java")
     apply(plugin = "org.springframework.boot")
     apply(plugin = "io.spring.dependency-management")
 
-    repositories {
-        mavenCentral()
-    }
-
+    // ────────────────────────────────────────────────────────────────────────
+    // 2) 공통 의존성 선언
+    // ────────────────────────────────────────────────────────────────────────
     dependencies {
         // Spring
-        implementation(libs.spring.boot.starter)
-        implementation(libs.spring.boot.devtools)
-        implementation(libs.spring.boot.starter.data.jpa) // JPA
-        implementation(libs.spring.boot.starter.data.redis) // REDIS
-        testImplementation(libs.spring.boot.starter.test) // JUnit5
+        implementation("org.springframework.boot:spring-boot-starter:3.2.5")
+        implementation("org.springframework.boot:spring-boot-devtools:3.2.5")
+        implementation("org.springframework.boot:spring-boot-starter-web:3.2.5")
+        implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.5") // JPA
+        implementation("org.springframework.boot:spring-boot-starter-data-redis:3.2.5") // REDIS
+        testImplementation("org.springframework.boot:spring-boot-starter-test:5.10.2") // JUnit5
 
         // Lombok
-        implementation(libs.lombok)
-        annotationProcessor(libs.lombok)
+        implementation("org.projectlombok:lombok:1.18.30")
+        annotationProcessor("org.projectlombok:lombok:1.18.30")
 
         // PostgreSQL
-        runtimeOnly(libs.postgresql)
-    }
-
-    java {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
+        runtimeOnly("org.postgresql:postgresql:42.7.3")
     }
 }
